89e24c6d348d74d8615b221462aa11f116cbcb18
RonexLemon <ronexondimu@gmail.com>
2023-02-10 14:40:31 +0300

    initialize nft contract

---
+++ chain/contracts/NFTMarketPlace.sol
@@ -0,0 +1,10 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+contract NFTMarketPlace {
+    
+}

f626c89b74aee2f88724f67b6e272b4a879188f0
RonexLemon <ronexondimu@gmail.com>
2023-02-11 05:32:49 +0300

    configure hardhat

--- chain/hardhat.config.js
+++ chain/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path: ".env"});
+const KEY = process.env.PRIVATE_KEY;
+const ALCHEMY = process.env.ALCHEMY_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    Goerli:{
+      url: ALCHEMY,
+      accounts: [KEY],
+    },
+  },
 };

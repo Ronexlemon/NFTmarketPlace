3d599bde9dd7a9d939b7644fa66fbd4076dd0d61
RonexLemon <ronexondimu@gmail.com>
2023-02-17 15:45:51 +0300

    fixing map

--- marketplace/src/pages/MarketPlace.jsx
+++ marketplace/src/pages/MarketPlace.jsx
@@ -1,14 +1,103 @@
-import React from "react";
+import React,{useContext,useEffect, useState} from "react";
+import { AppContext } from "../Contexts/AppContexts";
+import { marketPlaceAbi } from "../components/abi/marketplaceAbi";
+import axios from "axios";
+import NFTTile from "./NFTTile";
+import { ethers } from "ethers";
+const  MarketPlace =()=>{
 
-export default function MarketPlace(){
+    // const [data, updateData] = useState(sampleData);
+    // const [dataFetched, updateFetched] = useState(false);
+    // const [nft,setNft] = useState(sampleData);
+    const{
+        Contract,
+        connectWallet,
+        userAccount,
+        nftMarketPlaceContract,
+        // marketPlaceAbi,
+    
+    } = useContext(AppContext);
+    
+    
+    const sampleData = [
+        {
+            "name": "NFT#1",
+            "description": "Alchemy's First NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+        },
+        {
+            "name": "NFT#2",
+            "description": "Alchemy's Second NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        },
+        {
+            "name": "NFT#3",
+            "description": "Alchemy's Third NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        },
+    ];
+    const [data, updateData] = useState(sampleData);
+const [dataFetched, updateFetched] = useState(false);
 
-    return(
-        <div className="h-screen  bg-black text-white text-xs">
-            <div className="h-full flex justify-center items-center">
-            <h1>Welcome to MarketPlace</h1>
-            </div>
-            
+async function getAllNFTs() {
+    const provider = await connectWallet();
+    const contract = new Contract(nftMarketPlaceContract, marketPlaceAbi, provider)
+    let transaction = await contract.getAllNFTS();
+    
 
-        </div>
-    )
+    //Fetch all the details of every NFT from the contract and display
+    const items = await Promise.all(transaction.map(async i => {
+        const tokenURI = await contract.tokenURI(i.tokenId);
+        let meta = await axios.get(tokenURI);
+        meta = meta.data;
+
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: meta.image,
+            name: meta.name,
+            description: meta.description,
+        }
+        return item;
+    }))
+
+    updateFetched(true);
+    updateData(items);
 }
+    
+useEffect(()=>{
+ getAllNFTs();
+},[])
+return (
+    <div>
+        
+        <div className="flex flex-col place-items-center mt-20">
+            <div className="md:text-xl font-bold text-white">
+                Top NFTs
+            </div>
+            <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                {data.map((value, index) => {
+                    return <NFTTile data={value} key={index}></NFTTile>;
+                })}
+            </div>
+        </div>            
+    </div>
+);
+
+}
+export default MarketPlace;
